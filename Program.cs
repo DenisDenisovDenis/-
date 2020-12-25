9a40d213ed9242479aa4af2d259215e19c1539e9
DenisDenisovDenis <76570217+DenisDenisovDenis@users.noreply.github.com>
2020-12-25 09:38:25 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -319,3 +319,43 @@ public class RectangleArea
         Console.ReadLine();
     }
 }
+
+namespace inputArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Столбцов: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Строк: ");
+            int y = int.Parse(Console.ReadLine());
+            int[,] mas = new int[x, y];
+            Console.WriteLine();
+ 
+            Console.WriteLine("Заполни матрицу");
+ 
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write("mas["+i+","+j+"]: ");
+                    mas[i, j] = int.Parse(Console.ReadLine());
+                }       
+            }
+            Console.WriteLine();
+ 
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write(" mas[" + i + "," + j + "]: " + mas[i,j]+"\t");
+                }
+                Console.WriteLine();
+            }
+ 
+            Console.ReadLine();
+        }
+    }
+}
+Полезно ли:
